a6ad8b4965e98aef598517cfeda32948ca01999d
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-09 08:56:10 +0300

    register a blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -17,4 +17,9 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
 
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
     return app
