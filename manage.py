4b89008fadab832a8ef9f2143593fd7254e8eb7d
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-09 07:37:25 +0300

    setup the manager script

--- manage.py
+++ manage.py
@@ -1,4 +1,20 @@
 from  flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Server
+
+# Creating app instance
+app = create_app('production')
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+
+if __name__ == '__main__':
+    manager.run()
